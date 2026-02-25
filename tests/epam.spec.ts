b8685bd372049c5e0c5ad7cea7eb73861c53831c
Azhilin <alexei.zhilin@gmail.com>
2026-02-26 00:36:26 +0100

    Create tests/epam.spec.ts

---
+++ tests/epam.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+// Test: EPAM Services -> Explore Our Client Work -> Verify Client Work text
+test('EPAM Services explore client work', async ({ page }) => {
+  // Steps will be added in subsequent edits per incremental write rules.
+});
